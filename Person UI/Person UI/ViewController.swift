998f3e2bc3b38bfbee98c2388172012bc4774153
Jakob Daugherty <jadppf@mail.missouri.edu>
2017-06-29 11:24:17 -0500

    Init Xcode project

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Jakob Daugherty on 6/29/17.
+//  Copyright © 2017 JakobDaugherty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
